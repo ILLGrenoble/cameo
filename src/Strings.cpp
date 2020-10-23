4e686cd52a19ddea96ce1265664afe79c4d8b4ec
legoc <legoc@ill.eu>
2020-10-23 14:35:56 +0200

    (split) Added strings classes in C++

--- src/Strings.cpp
+++ src/Strings.cpp
@@ -16,18 +16,26 @@
 
 #include "Strings.h"
 #include "BadFormatException.h"
-#include <iostream>
+#include <regex>
 
 using namespace std;
 
 namespace cameo {
 
+bool Name::check(const std::string& str) {
+
+	regex nameRegex("[a-zA-Z0-9\\-_]+");
+	std::cmatch m;
+
+	return regex_match(str.c_str(), m, nameRegex);
+}
+
 std::vector<std::string> split(const std::string& str, char c) {
 
 	vector<string> result;
 
-	int lastIndex = 0;
-	int index = str.find(c);
+	string::size_type lastIndex = 0;
+	string::size_type index = str.find(c);
 	while (index != string::npos) {
 		result.push_back(str.substr(lastIndex, index - lastIndex));
 		lastIndex = index + 1;
@@ -57,7 +65,7 @@ Endpoint Endpoint::parse(const std::string& str) {
 		throw new BadFormatException("Bad format for endpoint " + str);
 	}
 
-	string substr = str.substr(7);
+	string substr = str.substr(6);
 	vector<string> tokens = split(substr, ':');
 
 	if (tokens.size() != 2) {
@@ -81,4 +89,144 @@ std::string Endpoint::toString() const {
 	return string("tcp://") + m_address + ":" + to_string(m_port);
 }
 
+NameId::NameId(const std::string& name, int id) :
+	m_name(name),
+	m_id(id) {
 }
+
+const std::string& NameId::getName() const {
+	return m_name;
+}
+
+int NameId::getId() const {
+	return m_id;
+}
+
+NameId NameId::parse(const std::string& str) {
+
+	vector<string> tokens = split(str, '.');
+
+	if (tokens.size() > 2) {
+		throw BadFormatException("Bad format for nameid " + str);
+	}
+
+	string name = tokens[0];
+	int id = -1;
+
+	if (tokens.size() == 2) {
+		try {
+			id = stoi(tokens[1]);
+		}
+		catch (...) {
+			throw new BadFormatException("Bad format for endpoint " + str);
+		}
+	}
+
+	return NameId(name, id);
+}
+
+std::string NameId::toString() const {
+
+	if (m_id > 0) {
+		return m_name + "." + to_string(m_id);
+	}
+	return m_name;
+}
+
+ApplicationIdentity::ApplicationIdentity(const NameId& nameId, const Endpoint& endpoint) :
+	m_nameId(nameId),
+	m_endpoint(endpoint) {
+}
+
+const NameId& ApplicationIdentity::getNameId() const {
+	return m_nameId;
+}
+
+const Endpoint& ApplicationIdentity::getEndpoint() const {
+	return m_endpoint;
+}
+
+ApplicationIdentity ApplicationIdentity::parse(const std::string& str) {
+
+	vector<string> tokens = split(str, '@');
+
+	if (tokens.size() != 2) {
+		throw BadFormatException("Bad format for application identity " + str);
+	}
+
+	return ApplicationIdentity(NameId::parse(tokens[0]), Endpoint::parse(tokens[1]));
+}
+
+std::string ApplicationIdentity::toString() const {
+	return m_nameId.toString() + "@" + m_endpoint.toString();
+}
+
+ApplicationAndStarterIdentities::ApplicationAndStarterIdentities(const ApplicationIdentity& application, const ApplicationIdentity& starter) :
+	m_application(application),
+	m_starter(starter) {
+}
+
+const ApplicationIdentity& ApplicationAndStarterIdentities::getApplication() const {
+	return m_application;
+}
+
+const ApplicationIdentity& ApplicationAndStarterIdentities::getStarter() const {
+	return m_starter;
+}
+
+ApplicationAndStarterIdentities ApplicationAndStarterIdentities::parse(const std::string& str) {
+
+	// The string is either <name>@<endpoint>:<name>@<endpoint> or <name>@<endpoint>:
+	// To separate the two identities, we search for the last : before the last @.
+
+	string::size_type firstIndex = str.find_first_of('@');
+
+	if (firstIndex == string::npos) {
+		throw new BadFormatException("Bad format for application and starter identities " + str);
+	}
+
+	string::size_type index = str.find_last_of('@');
+
+	if (index == firstIndex) {
+
+		// Format <name>@<endpoint>:
+		if (str[str.length() - 1] != ':') {
+			throw BadFormatException("Bad format for application and starter identities " + str);
+		}
+
+		string applicationString = str.substr(0, str.length() - 1);
+
+		ApplicationIdentity application = ApplicationIdentity::parse(applicationString);
+
+		return ApplicationAndStarterIdentities(application, application);
+	}
+	else {
+		// Format <name>@<endpoint>:<name>@<endpoint>
+		string substring = str.substr(0, index);
+		index = substring.find_last_of(':');
+
+		if (index == string::npos) {
+			throw BadFormatException("Bad format for application and starter identities " + str);
+		}
+
+		string applicationString = str.substr(0, index);
+		string starterString = str.substr(index + 1, str.length() - index);
+
+		ApplicationIdentity application = ApplicationIdentity::parse(applicationString);
+		ApplicationIdentity starter = ApplicationIdentity::parse(starterString);
+
+		return ApplicationAndStarterIdentities(application, starter);
+	}
+}
+
+std::string ApplicationAndStarterIdentities::toString() const {
+//	if (starter != null) {
+//		return application + ":" + starter;
+//	}
+//	return application + ":";
+
+	return "";
+}
+
+}
+
