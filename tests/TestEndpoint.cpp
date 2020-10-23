c49ff93934d249f7a9b7d88aa010f14d53cfb09c
legoc <legoc@ill.eu>
2020-10-23 17:14:23 +0200

    (split) Removed Some strings classes and preparing to JSON argument info

--- tests/TestEndpoint.cpp
+++ tests/TestEndpoint.cpp
@@ -9,21 +9,21 @@ int main(int argc, char *argv[]) {
 
 	Endpoint endpoint("gamma75", 9999);
 
-	CAMEO_ASSERT_TRUE("tcp://gamma75:9999" == endpoint.toString());
+	CAMEO_ASSERT_TRUE("gamma75:9999" == endpoint.toString());
 
-	endpoint = Endpoint::parse("tcp://gamma75:9999");
+	endpoint = Endpoint::parse("gamma75:9999");
 
 	CAMEO_ASSERT_TRUE("gamma75" == endpoint.getAddress());
 	CAMEO_ASSERT_EQUAL(9999, endpoint.getPort());
 
-	endpoint = Endpoint::parse("tcp://175.29.285.15:9999");
+	endpoint = Endpoint::parse("175.29.285.15:9999");
 
 	CAMEO_ASSERT_TRUE("175.29.285.15" == endpoint.getAddress());
 	CAMEO_ASSERT_EQUAL(9999, endpoint.getPort());
 
 	bool error = false;
 	try {
-		Endpoint::parse("tcp:/gamma75:9999");
+		Endpoint::parse("gamma:75:9999");
 	}
 	catch (...) {
 		error = true;
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
 
 	error = false;
 	try {
-		Endpoint::parse("tcp://gamma75:");
+		Endpoint::parse("gamma75:");
 	}
 	catch (...) {
 		error = true;
