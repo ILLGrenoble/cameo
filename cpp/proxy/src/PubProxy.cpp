e2e4e9d7ae2b18e04385c00d808a0f4a238a1ceb
legoc <legoc@ill.eu>
2022-02-10 16:39:29 +0100

    Created pub proxy

---
+++ cpp/proxy/src/PubProxy.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 Institut Laue-Langevin
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "License");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
+#include <string>
+#include <iostream>
+#include <zmq.hpp>
+
+int main(int argc, char *argv[]) {
+
+	if (argc < 3) {
+		std::cout << "Usage: <front port> <back port>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string frontPort{argv[1]};
+	std::string frontAddress("tcp://*:");
+	frontAddress += frontPort;
+
+	std::string backPort{argv[1]};
+	std::string backAddress("tcp://*:");
+	backAddress += backPort;
+
+	// Prepare our context and sockets.
+	zmq::context_t context(1);
+	zmq::socket_t frontend(context, ZMQ_XSUB);
+	//set hwm
+	frontend.bind(frontAddress);
+
+	zmq::socket_t backend(context, ZMQ_XPUB);
+	//set hwm
+	backend.bind(backAddress);
+
+	zmq::proxy(frontend, backend);
+
+	return EXIT_SUCCESS;
+}
