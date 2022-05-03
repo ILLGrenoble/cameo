02b4b102ca1503e1074a3ee1102698750a1cf8ad
legoc <legoc@ill.eu>
2022-05-03 16:16:04 +0200

    Corrected cmake files for building packages

--- build_and_package.sh
+++ build_and_package.sh
@@ -12,27 +12,29 @@ function mvPack(){
 }
 
 #--------------- JAVA
-for source_dir in cameo-server-jzmq cameo-console-jzmq
+for source_dir in java/server-jzmq java/console-jzmq
 do
 	echo "COMPONENT: $source_dir"
 	build_dir=$BASE_BUILD_DIR/$source_dir
 	cmake -S $source_dir -B $build_dir/ || exit 1
 	cmake --build $build_dir || exit 1
-	cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 2
+	cpack -V --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 2
 	mvPack
 done
 
 #--------------- API
 #---------- C++
-build_dir=$BASE_BUILD_DIR/cameo-api-cpp/
-cmake -S cameo-api-cpp -B $build_dir -DCMAKE_INSTALL_PREFIX=/usr/ # with this it adds x86_64-linux-gnu to the path when running cpack
+source_dir=cpp/api
+build_dir=$BASE_BUILD_DIR/$source_dir
+cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=/usr/ # with this it adds x86_64-linux-gnu to the path when running cpack
 cmake --build $build_dir 
-cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging 
+cpack -V --config $build_dir/CPackConfig.cmake -B $build_dir/packaging 
 mvPack
 
 #---------- Python
-build_dir=$BASE_BUILD_DIR/cameo-api-python/
-cmake -S cameo-api-python -B $build_dir -DCMAKE_PREFIX_PATH=$BASE_BUILD_DIR/cameo-api-cpp/ || exit 1
+source_dir=python/api
+build_dir=$BASE_BUILD_DIR/$source_dir
+cmake -S $source_dir -B $build_dir -DCMAKE_PREFIX_PATH=$BASE_BUILD_DIR/$source_dir || exit 1
 cmake --build $build_dir || exit 1
-cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 1
+cpack -V --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 1
 mvPack
