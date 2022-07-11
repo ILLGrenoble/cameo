e604fcdba959682381e44cca1b19f286e978b4a1
Shervin Nourbakhsh <shervin.nourbakhsh@ill.fr>
2022-07-11 09:25:48 +0200

    Adding cpack config for RPM

--- build_and_package.sh
+++ build_and_package.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+
+CPACK_GENERATORS="DEB;RPM"
 
 BASE_BUILD_DIR=${1:-/dev/shm/cameo/}
 [ "$BASE_BUILD_DIR" != "${BASE_BUILD_DIR#/}" ] || BASE_BUILD_DIR=$PWD/$BASE_BUILD_DIR
@@ -9,7 +12,7 @@ mkdir ${packages_dir} -p
 #mvn  install
 
 function mvPack(){
-	mv ${build_dir}/packaging/*.deb ${packages_dir}/
+	mv ${build_dir}/packaging/*.{rpm,deb} ${packages_dir}/
 }
 
 #--------------- JAVA
@@ -17,9 +20,9 @@ for source_dir in java/server-jzmq java/console-jzmq
 do
 	echo "COMPONENT: $source_dir"
 	build_dir=$BASE_BUILD_DIR/$source_dir
-	cmake -S $source_dir -B $build_dir/ || exit 1
-	cmake --build $build_dir || exit 1
-	cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 2
+	cmake -S $source_dir -B $build_dir/
+	cmake --build $build_dir 
+	cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ -G $CPACK_GENERATORS
 	mvPack
 done
 
@@ -31,7 +34,7 @@ source_dir=cpp/proxy
 build_dir=$BASE_BUILD_DIR/$source_dir
 cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=/usr/ -DCMAKE_MODULE_PATH=$source_dir/../ # with this it adds x86_64-linux-gnu to the path when running cpack
 cmake --build $build_dir 
-cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging 
+cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging -G $CPACK_GENERATORS
 mvPack
 
 #--------------- API
@@ -39,14 +42,14 @@ mvPack
 build_dir=$BASE_BUILD_DIR/$source_dir_cpp
 cmake -S $source_dir_cpp -B $build_dir -DCMAKE_INSTALL_PREFIX=/usr/ # with this it adds x86_64-linux-gnu to the path when running cpack
 cmake --build $build_dir 
-cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging 
+cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging -G $CPACK_GENERATORS
 mvPack
 
 #---------- Python
 source_dir=python/api
 build_dir=$BASE_BUILD_DIR/$source_dir
-cmake -S $source_dir -B $build_dir -DCMAKE_PREFIX_PATH=$BASE_BUILD_DIR/$source_dir_cpp || exit 1
-cmake --build $build_dir || exit 1
-cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ || exit 1
+cmake -S $source_dir -B $build_dir -DCMAKE_PREFIX_PATH=$BASE_BUILD_DIR/$source_dir_cpp
+cmake --build $build_dir
+cpack --config $build_dir/CPackConfig.cmake -B $build_dir/packaging/ -G $CPACK_GENERATORS
 mvPack
 
