c504f032fe31728766a0a371b599396472dfcd2e
legoc <legoc@ill.fr>
2024-09-30 15:55:16 +0200

    Corrected cmo.cmd

--- examples/cmo.cmd
+++ examples/cmo.cmd
@@ -7,7 +7,7 @@ if exist "%cameo_jeromq_jar%" (
   exit /b 0
 )
 
-set "cameo_jzmq_jar=examples\java\jeromq\target\cameo-examples-jzmq-full.jar"
+set "cameo_jzmq_jar=examples\java\jzmq\target\cameo-examples-jzmq-full.jar"
 if exist "%cameo_jzmq_jar%" (
   java -jar java\console\jzmq\target\cameo-console-jzmq-3.0.0-full.jar %*
   exit /b 0
