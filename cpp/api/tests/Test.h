f42c9e4dc5706457edce4b4b098d524b2c87ace1
legoc <legoc@ill.fr>
2024-10-08 11:13:03 +0200

    Test.h removes pretty function

--- cpp/api/tests/Test.h
+++ cpp/api/tests/Test.h
@@ -24,8 +24,6 @@
     throw std::runtime_error(   std::string( __FILE__ )             \
                               + std::string( ":" )                  \
                               + std::to_string( __LINE__ )          \
-                              + std::string( " in " )               \
-                              + std::string( __PRETTY_FUNCTION__ )  \
     );                                                              \
   }                                                                 \
 }
@@ -37,8 +35,6 @@
     throw std::runtime_error(   std::string( __FILE__ )             \
                               + std::string( ":" )                  \
                               + std::to_string( __LINE__ )          \
-                              + std::string( " in " )               \
-                              + std::string( __PRETTY_FUNCTION__ )  \
                               + std::string( ": " )                 \
                               + std::to_string( ( x ) )             \
                               + std::string( " != " )               \
