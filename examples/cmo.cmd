5ad7887db012ac593fac97643b8f3fbed3c2ff3f
legoc <legoc@ill.fr>
2025-10-24 17:00:10 +0200

    Corrected cmo jar version in examples

--- examples/cmo.cmd
+++ examples/cmo.cmd
@@ -3,13 +3,13 @@
 
 set "cameo_jeromq_jar=examples\java\jeromq\target\cameo-examples-jeromq-full.jar"
 if exist "%cameo_jeromq_jar%" (
-  java -jar java\console\jeromq\target\cameo-console-jeromq-3.0.0-full.jar %*
+  java -jar java\console\jeromq\target\cameo-console-jeromq-3.0.1-full.jar %*
   exit /b 0
 )
 
 set "cameo_jzmq_jar=examples\java\jzmq\target\cameo-examples-jzmq-full.jar"
 if exist "%cameo_jzmq_jar%" (
-  java -jar java\console\jzmq\target\cameo-console-jzmq-3.0.0-full.jar %*
+  java -jar java\console\jzmq\target\cameo-console-jzmq-3.0.1-full.jar %*
   exit /b 0
 )
 
