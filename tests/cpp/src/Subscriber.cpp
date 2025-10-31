bb9c5011d88d09476621be50337e7cf24efadbb1
legoc <legoc@ill.fr>
2025-10-31 13:39:08 +0100

    Some scope correction in C++ tests

--- tests/cpp/src/Subscriber.cpp
+++ tests/cpp/src/Subscriber.cpp
@@ -17,46 +17,47 @@ using namespace cameo;
 int main(int argc, char *argv[]) {
 
 	This::init(argc, argv);
+	{
+		string applicationName;
 
-	string applicationName;
+		if (argc > 2) {
+			applicationName = argv[1];
 
-	if (argc > 2) {
-		applicationName = argv[1];
+			cout << "Publisher application is " + applicationName << endl;
 
-		cout << "Publisher application is " + applicationName << endl;
-
-	} else {
-		cerr << "Arguments: [application name]" << endl;
-		return -1;
-	}
-
-	Server& server = This::getServer();
-
-	unique_ptr<App> publisherApplication = server.connect(applicationName);
-
-	// Create a subscriber to the application applicationName.
-	unique_ptr<coms::Subscriber> subscriber = coms::Subscriber::create(*publisherApplication, "publisher");
-	subscriber->init();
-
-	if (!subscriber) {
-		cout << "Subscriber error" << endl;
-		return -1;
-	}
-
-	cout << "Synchronized with 1 publisher" << endl;
-
-	This::setRunning();
-
-	// Receive data.
-	while (true) {
-		optional<tuple<string, string>> data = subscriber->receiveTwoParts();
-		if (!data.has_value()) {
-			break;
+		} else {
+			cerr << "Arguments: [application name]" << endl;
+			return -1;
 		}
-		cout << "Received " << get<0>(data.value()) << ", " << get<1>(data.value()) << endl;
-	}
 
-	cout << "Finished the application" << endl;
+		Server& server = This::getServer();
+
+		unique_ptr<App> publisherApplication = server.connect(applicationName);
+
+		// Create a subscriber to the application applicationName.
+		unique_ptr<coms::Subscriber> subscriber = coms::Subscriber::create(*publisherApplication, "publisher");
+		subscriber->init();
+
+		if (!subscriber) {
+			cout << "Subscriber error" << endl;
+			return -1;
+		}
+
+		cout << "Synchronized with 1 publisher" << endl;
+
+		This::setRunning();
+
+		// Receive data.
+		while (true) {
+			optional<tuple<string, string>> data = subscriber->receiveTwoParts();
+			if (!data.has_value()) {
+				break;
+			}
+			cout << "Received " << get<0>(data.value()) << ", " << get<1>(data.value()) << endl;
+		}
+
+		cout << "Finished the application" << endl;
+	}
 
 	This::terminate();
 
